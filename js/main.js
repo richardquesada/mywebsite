9ef0fe6de6a70a267af86cd73302849e718b7c63
Richard Quesada <richard.quesada@hotmail.com>
2019-01-03 13:34:59 -0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,145 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+    home.classList.add("blur");
+
+    //SET MENU STATE
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+    home.classList.remove("blur");
+    showMenu = false;
+  }
+}
+
+var canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext("2d");
+
+var mouse = {
+  x: undefined,
+  y: undefined
+};
+
+var maxRadius = 40;
+var minRadius = 2;
+
+var colorArray = ["#105B63", "#FFAD5", "#FFD34E", "#DB9E36", "#BD4932"];
+
+window.addEventListener("mousemove", function(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+window.addEventListener("resize", function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  init();
+});
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.minRadius = radius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+  c.globalAlpha = 0.2;
+
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  };
+
+  this.update = function() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    // interactivity
+    if (
+      mouse.x - this.x < 35 &&
+      mouse.x - this.x > -35 &&
+      mouse.y - this.y < 35 &&
+      mouse.y - this.y > -35
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius += 1;
+      }
+    } else if (this.radius > this.minRadius) {
+      this.radius -= 1;
+    }
+
+    this.draw();
+  };
+}
+
+var circleArray = [];
+
+function init() {
+  4;
+  circleArray = [];
+  for (var i = 0; i < 1000; i++) {
+    var radius = Math.random() * 0.1 + 1;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5) * 2;
+    var dy = (Math.random() - 0.5) * 2;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (var i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+}
+
+init();
+animate();
+
+/*for (var i = 0; i < 9999; i++) {
+	var x = Math.random() * window.innerWidth;
+	var y = Math.random() * window.innerHeight;
+	c.beginPath();
+	c.arc(x, y, 30, 0, Math.PI * 2, false);
+	c.strokeStyle ='blue';
+	c.stroke();
+
+}*/
